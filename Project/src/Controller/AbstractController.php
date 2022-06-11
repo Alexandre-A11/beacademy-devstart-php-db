f979c26f81e1c9d432af1f98ee71d46caea897be
Alexandre Almeida <alee.wxa@gmail.com>
2022-06-11 19:39:45 -0300

    adiciona bootstrap

--- Project/src/Controller/AbstractController.php
+++ Project/src/Controller/AbstractController.php
@@ -6,6 +6,11 @@ namespace App\Controller;
 
 abstract class AbstractController {
     public function render(string $viewName, $data = null): void {
+        include "../src/View/_partials/head.php";
         include "../src/View/{$viewName}.php";
+        include "../src/View/_partials/footer.php";
+        // include dirname(__DIR__) . "/View/_partials/head.php";
+        // include dirname(__DIR__) . "/View/{$viewName}.php";
+        // include dirname(__DIR__) . "/View/_partials/footer.php";
     }
 }
